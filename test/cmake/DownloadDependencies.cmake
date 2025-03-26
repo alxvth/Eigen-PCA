9d0b18f7aeaf0e64be6f0081be4b100258320421
Alexander Vieth <vieth.alexander@gmx.net>
2025-03-26 15:00:06 +0100

    update catch 2

--- test/cmake/DownloadDependencies.cmake
+++ test/cmake/DownloadDependencies.cmake
@@ -16,7 +16,7 @@ FetchContent_MakeAvailable(Eigen3)
 FetchContent_Declare(
     Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2
-    GIT_TAG v3.7.1
+    GIT_TAG v3.8.0
     GIT_SHALLOW TRUE
     FIND_PACKAGE_ARGS
 )
